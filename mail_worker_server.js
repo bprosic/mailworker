207cce66840195ed194bdb8e98dfe3102b3f19d0
unknown <bprosic@itc203w10bp.pers.local>
2023-11-21 15:56:51 +0100

    no session

--- mail_worker_server.js
+++ mail_worker_server.js
@@ -1,7 +1,10 @@
+const { randomBytes } = require('crypto');
+
 const express = require('express'),
   app = express(),
   port = process.env.PORT || 3015,
   cors = require('cors'),
+  cookieParser = require('cookie-parser'), // this is for CSRF
   bodyParser = require('body-parser'),
   CREOLIC_FRONTEND_DNS_ENDPOINT = process.env.CREOLIC_FRONTEND_DNS_ENDPOINT,
   CREOLIC_FRONTEND_LOCAL_ENDPOINT = process.env.CREOLIC_FRONTEND_LOCAL_ENDPOINT,
@@ -11,19 +14,75 @@ const express = require('express'),
     process.env.CREOLIC_BACKEND_LOCAL_ENDPOINT_1,
   CREOLIC_BACKEND_LOCAL_ENDPOINT_2 =
     process.env.CREOLIC_BACKEND_LOCAL_ENDPOINT_2,
+  CREOLIC_BACKEND_LOCAL_ENDPOINT_3 =
+    process.env.CREOLIC_BACKEND_LOCAL_ENDPOINT_3,
   routes = require('./routes'),
   https = require('https'),
   fs = require('fs'),
   uuid = require('uuid-v4'),
   helmet = require('helmet'),
-  compression = require('compression');
+  compression = require('compression'),
+  csrf = require('csrf'),
+  expressSession = require('express-session'); // for csrf
 
 app.use(compression());
+app.use(helmet());
+
 app.use(
   bodyParser.urlencoded({
     extended: true,
   })
 );
+app.use(bodyParser.json()); // csrf
+app.use(cookieParser()); // csrf
+//https://levelup.gitconnected.com/how-to-implement-csrf-tokens-in-express-f867c9e95af0
+// set up the cookie for the session
+app.use(
+  expressSession({
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+      secure: false,
+      path: '/',
+      name: 'creolic_session',
+      sameSite: 'lax',
+      maxAge: 24 * 60 * 60 * 1000,
+      secret: 'MAKE_THIS_SECRET_SECURE',
+    },
+  })
+);
+
+const attachCsrfToken = (req, res, next) => {
+  console.log('req.session', req.session);
+  if (req.session.csrf === undefined) {
+    console.log('usao u req.session.csrf');
+    req.session.csrf = randomBytes(100).toString('base64');
+  }
+  // const csrfTokenToSendToFrontEnd = req.csrfToken();
+  // console.log('csrfTokenToSendToFrontEnd: ', csrfTokenToSendToFrontEnd);
+  // // this cookie must be XSRF-TOKEN, because already defined as default in Angular.
+  // res.cookie('XSRF-TOKEN', csrfTokenToSendToFrontEnd);
+  next();
+
+  // next();
+  // var tokenFrontEnd = req.csrfToken();
+  // res.cookie('xsrf-token', tokenFrontEnd); // do not change def names
+  // res.locals._csrf = tokenFrontEnd; // make the token available to all views. Do not change _csrf name
+  // console.log(tokenFrontEnd);
+  // next();
+};
+
+// error handler
+app.use((err, req, res, next) => {
+  if (err.code !== 'EBADCSRFTOKEN') {
+    return next(err);
+  }
+  res.status(403).json({
+    message: 'error',
+  });
+});
+
 // #9
 app.use(function (req, res, next) {
   req.headers.origin = req.headers.origin || req.headers.host;
@@ -38,6 +97,7 @@ const allowedOrigins = [
   `${CREOLIC_FRONTEND_IP_ENDPOINT2}`, // frontend 192.168.162.184
   `${CREOLIC_BACKEND_LOCAL_ENDPOINT_1}`, // backend #9
   `${CREOLIC_BACKEND_LOCAL_ENDPOINT_2}`, // backend #9
+  `${CREOLIC_BACKEND_LOCAL_ENDPOINT_3}`, // backend #9 // delete this for production
 ]; // you have to include both server and client
 const corsOptions = {
   origin: (origin, cb) => {
@@ -49,7 +109,7 @@ const corsOptions = {
       cb(null, true);
     } else {
       console.log('origin: ', origin);
-      cb(new Error('Not allowed by CORS'));
+      cb(new Error('Get back! Not allowed by CORS.'));
     }
   },
   methods: ['GET', 'POST', 'DELETE', 'UPDATE', 'PUT', 'PATCH'],
@@ -60,7 +120,12 @@ app.use(cors(corsOptions));
 // init middleware from body - to take from body parser
 app.use(express.json({ extended: false }));
 
-app.use('/', routes);
+// app.use('/creolic/online_form', csrfProtect, function (req, res) {
+//   console.log('usaoo.');
+//   res.status(200).send(req.csrfToken());
+// });
+
+app.use('/', attachCsrfToken, routes);
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
--- routes/creolicRoute.js
+++ routes/creolicRoute.js
@@ -3,18 +3,35 @@ const express = require('express'),
   { validateFormData } = require('./FormFunctions/validateFormData');
 // express routes, - and . are interpreted literally
 router.get('/is_server_online', (req, res) => {
-  res.status(200).send('sve ok');
+  res.status(200).send({ code: 200, msg: 'ok' });
+});
+
+router.get('/check_status', (req, res) => {
+  res.status(200).send({ code: 200, msg: { csrf: req.session.csrf } });
 });
 
 router.post('/online_form', async (req, res) => {
   let formData = req.body; // { name: '', email: '', phone: '', message: '' }
-  let isFormValid = await validateFormData(formData);
+  console.log('req body', req.body);
+  if (req.session.csrf !== req.body.token) {
+    console.log('req.session.csrf', req.session.csrf);
+    console.log('req.body.token', req.body.token);
+    return res
+      .status(200)
+      .send({ code: 403, msg: 'Csrf token not valid', detail: null });
+  }
+  // remove that key - token
+  let newArray = formData.forEach((element) => {
+    delete element.token;
+  });
+  console.log('newArray', newArray);
+  let isFormValid = await validateFormData(newArray);
   if (!isFormValid.valid) {
     res
       .status(200)
       .send({ code: 404, msg: 'Form not valid', detail: isFormValid.error });
   } else {
-    res.status(200).send({ code: 200, msg: 'Form valid', details: req.body });
+    res.status(200).send({ code: 200, msg: 'Form valid', details: newArray });
   }
 });
 
